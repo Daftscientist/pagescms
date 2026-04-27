6846e9a46dab4e5bf2778734cd089cda7333e24e
Leo Johnston Mesia <67394753+Daftscientist@users.noreply.github.com>
2026-04-27 16:24:52 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,22 @@ set -e
 
 if [ "${RUN_MIGRATIONS:-true}" = "true" ]; then
   echo "Running database migrations..."
-  npm run db:migrate
+
+  if [ -f pnpm-lock.yaml ]; then
+    pnpm run db:migrate
+  elif [ -f yarn.lock ]; then
+    yarn db:migrate
+  else
+    npm run db:migrate
+  fi
 fi
 
 echo "Starting Pages CMS..."
-exec npm run start
+
+if [ -f pnpm-lock.yaml ]; then
+  exec pnpm run start
+elif [ -f yarn.lock ]; then
+  exec yarn start
+else
+  exec npm run start
+fi
